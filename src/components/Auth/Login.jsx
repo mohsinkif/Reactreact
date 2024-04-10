df2f644d60acf3012d2c87071830135e582add7d
mohsinkif <mohsinullah68@gmail.com>
2024-04-10 16:46:00 -0700

    Login work in login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import App from '../../App';
+import axios from 'axios'
 
 const Login = () => {
   const [state, setState] = useState(false);
-
-  const handleSubmit = (event) => {
+  const [formData,setFormData]=useState({
+    email: '',
+    password:'',
+  })
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,[e.target.name]:e.target.value,
+    });
+  }
+  const handleSubmit = async (event) => {
     event.preventDefault(); 
-    
-    setState(true);
+    try{
+      const response= await axios.post('http://127.0.0.1:8001/login',formData)
+      console.log(response.data);
+      setState(true);
+    }catch(error){
+      console.error('Error Login IDK');
+    }
   };
 
   if (state) {
@@ -28,11 +42,11 @@ const Login = () => {
                 <div className="divide-y divide-gray-200">
                   <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                     <div className="relative">
-                      <input required autoComplete="off" id="email" name="email" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="Email address" />
+                      <input required autoComplete="off" id="email" name="email" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="Email address" onChange={handleChange} />
                       <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
                     </div>
                     <div className="relative">
-                      <input required autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="Password" />
+                      <input required autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" placeholder="Password" onChange={handleChange} />
                       <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                     </div>
                     <div className="relative">
